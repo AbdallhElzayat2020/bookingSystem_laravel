34d3eaaa1d813f13d48cd6dbbe8deca02a95c27a
Abdallh ELzayat <abdallhelzayat900@gmail.com>
2025-04-26 10:32:14 +0300

    Add initial layout files for admin dashboard including header, footer, sidebar, and scripts

---
+++ resources/views/admin/layouts/master.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+@include('admin.layouts.head')
+<body>
+<div id="app">
+    <div class="main-wrapper main-wrapper-1">
+        <!-- navbar -->
+        <div class="navbar-bg"></div>
+        @include('admin.layouts.header')
+        <!-- sidebar -->
+        @include('admin.layouts.sidebar')
+
+        <!-- Main Content -->
+        <div class="main-content">
+
+
+            @yield('content')
+        </div>
+
+        @include('admin.layouts.footer')
+
+    </div>
+</div>
+
+<!-- General JS Scripts -->
+@include('admin.layouts.scripts')
+</body>
+
+</html>
