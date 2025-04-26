139f97e39a4714c991cecca2b301e29bed574619
Abdallh ELzayat <abdallhelzayat900@gmail.com>
2025-04-26 07:50:58 +0300

    Implement admin authentication and dashboard routes

---
+++ app/Http/Controllers/Admin/AuthController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class AuthController extends Controller
+{
+    public function login(Request $request): View
+    {
+        return view('admin.auth.login');
+    }
+
+
+}
--- routes/admin.php
+++ routes/admin.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+use App\Http\Controllers\Admin\AuthController;
+use App\Http\Controllers\Admin\DashboardController;
+
+
+Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => ['user.type:admin']], function () {
+
+    Route::get('/dashboard', [DashboardController::class, 'dashboard'])->name('dashboard.index');
+
 });
+
+
+Route::get('/admin/login', [AuthController::class, 'login'])->name('admin.login');
